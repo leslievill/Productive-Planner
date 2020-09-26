ec6214ede0e4320e4702da247527fbdda5bc03c5
Leslie Villatoro <levill98@gmail.com>
2020-09-25 19:38:30 -0700

    set variables and var interval for time to display

---
+++ script.js
@@ -0,0 +1,27 @@
+// Moment.js
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+// Variables for each hour
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm A'));
+  }, 100);
